c1535834afab83307610fadc686d39d6aa83fd4f
Jiacheng Wang <jiachengwang@umass.edu>
2023-01-09 12:35:57 -0500

    update

--- MetaD_CS/MetaD_CS/Program.cs
+++ MetaD_CS/MetaD_CS/Program.cs
@@ -23,15 +23,15 @@ namespace ssh
 
             var name = Prompt.Input<string>("What's your name?");
             Console.WriteLine($"Hello, {name}!");
-            var secret = Prompt.Password("Type new password");
+            var pwd = Prompt.Password("Type new password");
             Console.WriteLine("Password OK");
 
             // Function list
             FunctionList();
 
 
-            // Console.WriteLine("Press \'q & Enter\' to quit.");
-            // while (Console.Read() != 'q') ;
+            Console.WriteLine("Press \'q & Enter\' to quit.");
+            while (Console.Read() != 'q') ;
         }
 
         static void FunctionList()
@@ -70,36 +70,36 @@ namespace ssh
                new PasswordAuthenticationMethod("jiachengwang", "wjc19950910")
             });
 
-            using (SshClient client = new(conInfo))
+            using SshClient client = new(conInfo);
+            
+            // Connect to server
+            client.Connect();
+            Console.WriteLine("Connected to baigroup");
+
+            // Create a shell stream for output
+            StringBuilder output = new();
+            ShellStream stream = client.CreateShellStream("stream", 0, 0, 0, 0, 587);
+
+            // Run command shotgun list-jobs
+            stream.WriteLine("shotgun list-jobs");
+            stream.Flush();
+            Thread.Sleep(5000);
+            var reader = new StreamReader(stream);
+
+            string? line;
+            int read_init = 0;
+            while ((line = reader.ReadLine()) != null)
             {
-                // Connect to server
-                client.Connect();
-                Console.WriteLine("Connected to baigroup");
+                if (line.Contains("Found")) { read_init = 1; }
 
-                // Create a shell stream for output
-                StringBuilder output = new();
-                ShellStream stream = client.CreateShellStream("stream", 0, 0, 0, 0, 587);
-
-                // Run command shotgun list-jobs
-                stream.WriteLine("shotgun list-jobs");
-                stream.Flush();
-                Thread.Sleep(5000);
-                var reader = new StreamReader(stream);
-
-                string? line;
-                int read_init = 0;
-                while ((line = reader.ReadLine()) != null)
+                if (read_init==1)
                 {
-                    if (line.Contains("Found")) { read_init = 1; }
-
-                    if (read_init==1)
-                    {
-                        string content = reader.ReadToEnd();
-                        string trimmed = content[..content.LastIndexOf("\r\n")];
-                        Console.WriteLine(trimmed);
-                        break;
-                    }
+                    string content = reader.ReadToEnd();
+                    string trimmed = content[..content.LastIndexOf("\r\n")];
+                    Console.WriteLine(trimmed);
+                    break;
                 }
+                
                 client.Disconnect();
             }
 
